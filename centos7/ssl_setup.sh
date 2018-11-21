e9bb0a8ec300cf484aa3fa4f114b7aa0bff1c69e
Shingo Yamada <uzuranoie@gmail.com>
2018-11-21 13:01:27 +0900

    Updated openssl command with sha256 option

--- centos7/ssl_setup.sh
+++ centos7/ssl_setup.sh
@@ -4,7 +4,7 @@ SSL_KEY_FILENAME="${SERVISE_DOMAIN}.key_${DATEYEAR}"
 ### Generate CSR ###
 mkdir -p /etc/pki/tls/csr
 cd /etc/pki/tls
-openssl genrsa -des3 -out ./private/${SSL_KEY_FILENAME} 2048
+openssl genrsa -des3 -out ./private/${SSL_KEY_FILENAME} 2048 -sha256
 openssl req -new -key ./private/${SSL_KEY_FILENAME} -out ./csr/${SERVISE_DOMAIN}.csr_${DATEYEAR}
 
 cp ./private/${SSL_KEY_FILENAME} ./private/${SSL_KEY_FILENAME}.ori
