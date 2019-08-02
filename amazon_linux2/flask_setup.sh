e1d0ae16f7bfffc399292d3454e70deb0eb55649
Shingo Yamada <yamada-shingo@pal-relations.co.jp>
2019-08-02 02:46:04 +0000

    Implemented to setup python3 and mod_wsgi

--- amazon_linux2/flask_setup.sh
+++ amazon_linux2/flask_setup.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+yum install -y python36-devel python36-libs python36-setuptools python36-pip
+yum install -y httpd-devel
+pip3 install mod_wsgi
+MOD_WSGI_PATH=`find /usr/local/lib64/python3.6/ -type f -name "mod_wsgi*.so"`
+cat >> /etc/httpd/conf.d/virtualhost.conf <<EOF
+
+LoadModule wsgi_module ${MOD_WSGI_PATH}
+<VirtualHost *:80>
+  ServerName example.com
+  DocumentRoot /var/www/sites/${SERVISE_DOMAIN}
+  WSGIScriptAlias / /var/www/sites/${SERVISE_DOMAIN}/adapter.wsgi
+  <Directory "/var/www/sites/${SERVISE_DOMAIN}/">
+    Order deny,allow
+    Allow from all
+  </Directory>
+</VirtualHost>
+
+EOF
+#systemctl httpd restart
